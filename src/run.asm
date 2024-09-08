7988fbe9389eb2afa4beca58987bd198a31ca9fe
thegoodstuffshack <thegoodstuffshack@gmail.com>
2024-09-08 16:32:41 +1000

    forgor file

---
+++ src/run.asm
@@ -0,0 +1,21 @@
+[bits 64]
+[default rel]
+jmp start
+
+signature: db 'BadApple'
+
+
+
+start:
+	; make/find frame buffer
+	; edit frame buffer
+	; swap buffers
+	; repeat
+
+	mov rax, -1
+	mov rbx, -1
+	mov rcx, -1
+	mov rdx, -1
+	jmp $
+
+
