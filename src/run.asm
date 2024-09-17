0774f2bc94d9c92038687ebecd873b7dc07c5ef6
thegoodstuffshack <thegoodstuffshack@gmail.com>
2024-09-17 17:10:47 +1000

    tested altering frame buffer and accessing given system struct

--- src/run.asm
+++ src/run.asm
@@ -4,21 +4,34 @@ jmp start
 
 signature: db 'BadApple'
 
+; pointer to struct in rbx -- need to update as needed
+; SystemInfoStruct {
+;	UINT64	Size
+;	VOID*	SystemTable
+;	VOID*	GOP_Interface
+;	VOID*	VRAM
+;	UINT32	ScreenWidth
+;	UINT32	ScreenHeight
+;	...
+; }
+
+
 start:
 	; make/find frame buffer
 	; edit frame buffer
 	; swap buffers
 	; repeat
 
-	mov rax, -1
-	mov rbx, -1
-	mov rcx, -1
-	mov rdx, -1
-
-
-
+.test_VRAM:
+	mov rsi, [rbx + 3*8]
+	mov ecx, [rbx + 4*8]
+	shl ecx, 7
+.loop:
+	mov dword [rsi], 0x0000FF00
+	add rsi, 4
+	loop .loop
 
 	cli
 	hlt
 
-
+SystemInfoStruct dq 0
