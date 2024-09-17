44f4b568cf0798d5d518b321148c098f5b73b113
thegoodstuffshack <thegoodstuffshack@gmail.com>
2024-09-18 00:12:33 +1000

    more screen teseting

--- src/run.asm
+++ src/run.asm
@@ -4,7 +4,7 @@ jmp start
 
 signature: db 'BadApple'
 
-; pointer to struct in rbx -- need to update as needed
+; pointer to struct given in rbx -- need to update struct as needed
 ; SystemInfoStruct {
 ;	UINT64	Size
 ;	VOID*	SystemTable
@@ -15,6 +15,13 @@ signature: db 'BadApple'
 ;	...
 ; }
 
+SIS_Size 			equ 0
+SIS_SystemTable 	equ 8
+SIS_GOP_Interface 	equ 16
+SIS_VRAM 			equ 24
+SIS_ScreenWidth 	equ 32
+SIS_ScreenHeight 	equ 36
+
 
 start:
 	; make/find frame buffer
@@ -22,16 +29,40 @@ start:
 	; swap buffers
 	; repeat
 
+
 .test_VRAM:
-	mov rsi, [rbx + 3*8]
-	mov ecx, [rbx + 4*8]
-	shl ecx, 7
+	mov rsi, [rbx + SIS_VRAM]
+	mov ecx, [rbx + SIS_ScreenWidth]
+	mov eax, [rbx + SIS_ScreenHeight]
+	mul ecx
+	mov ecx, eax
+
 .loop:
-	mov dword [rsi], 0x0000FF00
+	mov r12d, [BLUE]
+
+
+	mov dword [rsi], r12d
 	add rsi, 4
 	loop .loop
 
+	add byte [BLUE], 1
+	add byte [RED], 1
+	add byte [GREEN], 1
+	
+	xor eax, eax
+.wait:
+	inc eax
+	cmp eax, 0x1000000
+	jne .wait
+
+	jmp .test_VRAM
+
 	cli
 	hlt
+
+BLUE  db 255
+GREEN db 255
+RED   db 255
+reserved db 0
 
 SystemInfoStruct dq 0
