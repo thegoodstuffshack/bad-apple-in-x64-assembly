d669018c361a98475ae5faa9b9e3b1b0a1a3f692
thegoodstuffshack <thegoodstuffshack@gmail.com>
2024-09-17 17:07:11 +1000

    update cmd

--- run.bat
+++ run.bat
@@ -8,5 +8,4 @@ nasm -f bin src/run.asm -o HDA_DRIVE/programs/bad-apple.bin
 qemu-system-x86_64 ^
 -bios ovmf-x64/usr/share/edk2.git/ovmf-x64/OVMF-pure-efi.fd ^
 -drive format=raw,file=fat::rw::HDA_DRIVE ^
--drive format=raw,file=ovmf-x64/usr/share/edk2.git/ovmf-x64/UefiShell.iso ^
 -monitor stdio
