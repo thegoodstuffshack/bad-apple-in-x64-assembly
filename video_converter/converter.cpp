f357c5d053e87ecb1295700dfb2dd828836a4ace
thegoodstuffshack <thegoodstuffshack@gmail.com>
2025-10-05 22:03:02 +1100

    improve converter

--- video_converter/converter.cpp
+++ video_converter/converter.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <chrono>
+#include <vector>
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
@@ -13,24 +14,28 @@
 #define OUTPUTFOLDER "../CompressedFrameData.bin"
 
 
-void setBlock(unsigned int num, std::ofstream *file, unsigned int isLastBlock) {
+void setBlock(unsigned int num, std::vector<char> *buffer, unsigned int isLastBlock) {
 	div_t blocks = div(num, 65536 - 1);
-	for (;blocks.quot > 0; blocks.quot--) { 
-		*file << (unsigned char)0xFF << (unsigned char)0xFF;
+	for (;blocks.quot > 0; blocks.quot--) {
+		buffer->push_back((char)0xFF);
+		buffer->push_back((char)0xFF);
 		if (isLastBlock == 0 || (isLastBlock == 1 && blocks.rem != 0)) {
-			*file << (char)0 << (char)0; 
+			buffer->push_back((char)0);
+			buffer->push_back((char)0);
 		}
 	}
 	if (blocks.rem != 0) {
-		unsigned char lByte = blocks.rem & 255;
-		unsigned char uByte = blocks.rem >> 8;
-		*file << lByte << uByte;
+		buffer->push_back((char)(blocks.rem & 255));
+		buffer->push_back((char)(blocks.rem >> 8));
 	}
-	else if (isLastBlock == 0) { *file << (char)0 << (char)0; }
+	else if (isLastBlock == 0) {
+		buffer->push_back((char)0);
+		buffer->push_back((char)0);
+	}
 }
 
 // first block is always zero, one and zero blocks alternate
-void compressFrame(unsigned char *frame, std::ofstream *file) {
+void compressFrame(unsigned char *frame, std::vector<char> *buffer) {
 	unsigned int isFirstBlock = 0;
 	unsigned int zeroCount = 0;
 	unsigned int oneCount = 0;
@@ -38,9 +43,9 @@ void compressFrame(unsigned char *frame, std::ofstream *file) {
 		unsigned char pixel = frame[i];
 		if (pixel == 0) {
 			zeroCount++;
-			if (oneCount != 0) { 
-				if (isFirstBlock == 0) { isFirstBlock = 1;  setBlock(0, file, 0); }
-				setBlock(oneCount, file, 0); 
+			if (oneCount != 0) {
+				if (isFirstBlock == 0) { isFirstBlock = 1;  setBlock(0, buffer, 0); }
+				setBlock(oneCount, buffer, 0); 
 				oneCount = 0; 
 			}
 		}
@@ -48,14 +53,14 @@ void compressFrame(unsigned char *frame, std::ofstream *file) {
 			oneCount++;
 			if (zeroCount != 0) { 
 				if (isFirstBlock == 0) { isFirstBlock = 1; }
-				setBlock(zeroCount, file, 0); 
+				setBlock(zeroCount, buffer, 0);
 				zeroCount = 0; 
 			}
 		}
 		else std::cout << "frame not b and w" << std::endl;
 	}
-	if (zeroCount != 0) { setBlock(zeroCount, file, 1); }
-	else if (oneCount != 0) { setBlock(oneCount, file, 1); }
+	if (zeroCount != 0) { setBlock(zeroCount, buffer, 1); }
+	else if (oneCount != 0) { setBlock(oneCount, buffer, 1); }
 }
 
 int main(int argc, char** argv) {
@@ -79,9 +84,13 @@ int main(int argc, char** argv) {
 		cv::resize(baseFrame, resizedFrame, cv::Size (RESIZED_W, RESIZED_H), 0, 0, cv::InterpolationFlags::INTER_AREA);
 		cv::threshold(resizedFrame, bwFrame, 175, 255, cv::THRESH_BINARY);
 
-		compressFrame(bwFrame.data, &outputFile);
+		std::vector<char> buffer;
+		compressFrame(bwFrame.data, &buffer);
+		for (int i = 0; i < buffer.size(); i++) {
+			outputFile << buffer[i];
+		}
 
-		if (count%1000 == 0) {
+		if (count % 1000 == 0) {
 			std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now() - time).count() << std::endl;
 			time = std::chrono::system_clock::now();
 		}
